ea76f98d0c162171ac61e7469a1f2f2c1507ae3c
Paragoumba <paragoumba@gmail.com>
2020-02-17 18:19:28 +0100

    Added sources registering

--- Plugins/Music/src/main/java/fr/paragoumba/bost/music/QueuedAudioPlayerManager.java
+++ Plugins/Music/src/main/java/fr/paragoumba/bost/music/QueuedAudioPlayerManager.java
@@ -2,8 +2,17 @@ package fr.paragoumba.bost.music;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 
 public class QueuedAudioPlayerManager extends DefaultAudioPlayerManager {
+
+    public QueuedAudioPlayerManager(){
+
+        super();
+
+        AudioSourceManagers.registerRemoteSources(this);
+
+    }
 
     @Override
     protected AudioPlayer constructPlayer(){
