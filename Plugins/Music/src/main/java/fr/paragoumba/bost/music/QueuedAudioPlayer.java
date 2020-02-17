d2888786353e67dc75f05d9bd7d5c274b038e2ac
Paragoumba <paragoumba@gmail.com>
2020-02-17 18:18:30 +0100

    Improved many things

--- Plugins/Music/src/main/java/fr/paragoumba/bost/music/QueuedAudioPlayer.java
+++ Plugins/Music/src/main/java/fr/paragoumba/bost/music/QueuedAudioPlayer.java
@@ -3,7 +3,8 @@ package fr.paragoumba.bost.music;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackState;
+import fr.paragoumba.bost.Bot;
 
 import java.util.LinkedList;
 
@@ -76,7 +77,38 @@ public class QueuedAudioPlayer extends DefaultAudioPlayer {
 
     public LinkedList<AudioTrack> getQueuedTracks(){
 
-        return (LinkedList<AudioTrack>) queuedTracks.clone();
+        //return (LinkedList<AudioTrack>) queuedTracks.clone();
+        return queuedTracks;
+
+    }
+
+    public void removeTrack(int i){
+
+        queuedTracks.remove(i);
+
+    }
+
+    public void clearQueue(){
+
+        queuedTracks.clear();
+
+    }
+
+    public String getDuration(AudioTrack track){
+
+        String format = Bot.getConfig().getString("music.timeformat");
+
+        if (format == null){
+
+            format = "%mm%ss";
+
+        }
+
+        float duration = (float) track.getDuration() / 60000;
+
+        return format
+                .replaceAll("%m", String.valueOf((int) duration))
+                .replaceAll("%s", String.valueOf((int) (duration % 1 * 60)));
 
     }
 }
